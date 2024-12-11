382f13373e6134835e724a474730fa0fbd696f76
AyeshaZahid296 <ayeshazahid296gmail.com>
2024-12-11 00:54:27 -0500

    update responsive

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-// Select elements
 const counterValue = document.getElementById('counter-value');
 const decreaseBtn = document.getElementById('decrease');
 const resetBtn = document.getElementById('reset');
 const increaseBtn = document.getElementById('increase');
 
-// Initialize counter
 let count = 0;
 
-// Update Counter with Animation
 function updateCounter() {
   counterValue.textContent = count;
 
-  // Add "grow" class for the animation
   counterValue.classList.add('grow');
   setTimeout(() => {
     counterValue.classList.remove('grow');
-  }, 200); // Remove the class after 200ms
+  }, 200);
 }
 
-// Button Events
 decreaseBtn.addEventListener('click', () => {
   count--;
   updateCounter();
